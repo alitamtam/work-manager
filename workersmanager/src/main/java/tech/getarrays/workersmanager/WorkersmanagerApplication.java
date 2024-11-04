e784cefcb40b22f4eee94916c0e5cf92f503f047
alitamtam <alitamtam@oclock.school>
2024-11-04 13:52:09 +0100

    init commit

---
+++ workersmanager/src/main/java/tech/getarrays/workersmanager/WorkersmanagerApplication.java
@@ -0,0 +1,13 @@
+package tech.getarrays.workersmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersmanagerApplication.class, args);
+	}
+
+}
---
+++ workersmanager/src/test/java/tech/getarrays/workersmanager/WorkersmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.getarrays.workersmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
